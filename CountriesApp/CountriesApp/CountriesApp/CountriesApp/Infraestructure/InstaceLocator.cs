820b4eb7c128bfc2a203b68894f4641d06a7c97b
Jorge Monasterio <jorgehmg17@gmail.com>
2019-09-03 10:15:47 -0400

    Added the solution with the login page, also the pages for the tabbed page were added and a class of services too

---
+++ CountriesApp/CountriesApp/CountriesApp/CountriesApp/Infraestructure/InstaceLocator.cs
@@ -0,0 +1,17 @@
+﻿using CountriesApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountriesApp.Infraestructure
+{
+    public class InstaceLocator
+    {
+        public MainViewModel Main { get; set; }
+        public InstaceLocator()
+        {
+            Main = new MainViewModel();
+        }
+
+    }
+}
---
+++ CountriesApp/CountriesApp/CountriesApp/CountriesApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountriesApp.ViewModels
+{
+    public class MainViewModel
+    {
+        public LoginViewModel LoginView { get; set; }
+        public CountryViewModel CountryView { get; set; }
+        public static MainViewModel Main { get; set; }
+
+        public MainViewModel()
+        {
+            LoginView = new LoginViewModel();
+        }
+
+        public static MainViewModel GetInstace()
+        {
+            if (Main == null)
+            {
+                return new MainViewModel();
+            }
+            return Main;
+        }
+    }
+}
